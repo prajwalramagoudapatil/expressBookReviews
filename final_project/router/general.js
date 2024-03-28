bbd076814ca9060aacfef98ee0e81c53d18eb574
prajwalramagoudapatil <101803815+prajwalramagoudapatil@users.noreply.github.com>
2024-03-28 10:50:21 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,20 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+  if(!username || !password) {
+    return res.status(404).json({message:"Enter username and password"});
+  }
+  if( isValid(username) ) {
+    res.json({message:"User already exists"});
+    res.redirect("../customer/login");
+  }
+  
+  users.push({"name":username,"password":password});
+  console.log(users);
+  res.status(200).json({message: "User successfully registred. Now you can login"});
+  res.redirect( "../customer/login");
 });
 
 // Get the book list available in the shop
@@ -27,27 +40,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   for( key in books) {
-    if (books[key].author === req. ) {
-      res.send(books[key]);
+    if (books[key].author === req.params.author ) 
+       return res.send(books[key]);
   }
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   for( key in books) {
-    if(books[key].title === req. )
-      res.send(books[key];
+    if(books[key].title === req.params.title ) {
+        res.send(JSON.stringify(books[key])) ;
+    }
   }
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "No book found with given title."});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   
-  return res.status(200).send(books[req. ].reviews );
+  return res.status(200).send(books[req.params.isbn ].reviews );
 });
 
 module.exports.general = public_users;
