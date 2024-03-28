52436b29645089bb083696607e781d544439d130
prajwalramagoudapatil <101803815+prajwalramagoudapatil@users.noreply.github.com>
2024-03-28 12:46:03 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,41 +9,61 @@ public_users.post("/register", (req,res) => {
   //Write your code here
   let username = req.body.username;
   let password = req.body.password;
+  console.log(username, password);
   if(!username || !password) {
     return res.status(404).json({message:"Enter username and password"});
   }
   if( isValid(username) ) {
     res.json({message:"User already exists"});
-    res.redirect("../customer/login");
+    // res.redirect("/customer/login");
   }
   
   users.push({"name":username,"password":password});
   console.log(users);
   res.status(200).json({message: "User successfully registred. Now you can login"});
-  res.redirect( "../customer/login");
+//   res.redirect( "/customer/login");
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+function getBooks() {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+}
+public_users.get('/', async function (req, res) {
   //Write your code here
-  return res.status(200).send(books);
+  getBooks().then((books) => res.send(JSON.stringify(books)));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+function getByISBN(isbn) {
+    return new Promise((resolve, reject) => {
+        let isbnNum = parseInt(isbn);
+        if (books[isbnNum]) {
+            resolve(books[isbnNum]);
+        } else {
+            reject({status:404, message:`ISBN ${isbn} not found`});
+        }
+    })
+  }
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
-  
-  return res.status(200).send(books[req.params.isbn]);
+  getByISBN(req.params.isbn)
+  .then(
+      result => res.send(result),
+      error => res.status(error.status).json({message: error.message})
+  );
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  for( key in books) {
-    if (books[key].author === req.params.author ) 
-       return res.send(books[key]);
-  }
-    return res.status(300).json({message: "Yet to be implemented"});
+    public_users.get('/author/:author',function (req, res) {
+        const author = req.params.author;
+        getBooks()
+        .then((bookEntries) => Object.values(bookEntries))
+        .then((books) => books.filter((book) => book.author === author))
+        .then((filteredBooks) => res.send(filteredBooks));
+      });
 });
 
 // Get all books based on title
@@ -59,9 +79,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  
-  return res.status(200).send(books[req.params.isbn ].reviews );
+    const isbn = req.params.isbn;
+    getByISBN(req.params.isbn)
+    .then(
+        result => res.send(result.reviews),
+        error => res.status(error.status).json({message: error.message})
+    );
 });
 
 module.exports.general = public_users;
